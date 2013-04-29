73f8291a6ace1931729750162ebf6ad022b398df
Thumperrr <sbhall808@hotmail.com>
2013-04-29 10:11:51 -0400

    Example code

---
+++ ID20Reader/examples/ID20ReaderExample/ID20ReaderExample.ino
@@ -0,0 +1,21 @@
+#include <ID20Reader.h>
+
+int rx_pin = 9; //Data input pin
+int tx_pin = 8; //Unused, but must be defined. (Nothing is sent from the Arduino to the reader.)
+
+ID20Reader rfid(rx_pin, tx_pin); //Create an instance of ID20Reader.
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("RFID Reader - Swipe a card ~~~~~");
+}
+
+void loop() {
+  rfid.read(); //Receive a tag from the reader if available
+
+  if(rfid.available()) //a tag has been read
+  {
+    String code = rfid.get(); //Get the tag
+    Serial.println(code); //Print the tag to the serial monitor
+  }
+}
